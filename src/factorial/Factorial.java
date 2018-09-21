a139ea0f0dd49f13389a93890fda0c65690c6892
tpc <42519716+thomaspetersonchicuma@users.noreply.github.com>
2018-09-21 13:51:03 +0200

    Initial commit

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,40 @@
+
+package factorial;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author User
+ */
+public class Factorial {
+
+   
+    public static void main(String[] args) {
+       
+      // System.out.println(9);
+        
+        Vector vet = new Vector(1,2);
+//        
+//        vet.add("Mars");
+//        vet.add("Fred");
+//        vet.add("Fred");
+//        vet.add("Fred");
+//        vet.add("Fred");
+//         vet.add("Fred");
+//        
+//        
+//        
+//        System.out.print(vet.capacity());
+    }
+    
+    int impar(int n){
+        if (n < 0){
+            return -1;
+        }else{
+            return (n-1);
+        }
+    }
+    
+    
+}
